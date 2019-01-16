640d76a6b162ad05657ec185d03498fcae3e4366
Shashank Gaurav <shashank@cyware.com>
2019-01-16 08:37:50 +0530

    updated route file

--- route.js
+++ route.js
@@ -9,5 +9,5 @@ router.post('/social', (req, res) => {
   const template = social_templates.find(template => template.template_id === req.body.data.template)
   res.render(template.template_file, req.body.data)
 })
-
+console.log('here')
 module.exports = router;
