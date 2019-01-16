f505a0d7bd6c0a620e64301d9d106b1dafc7c329
Shashank Gaurav <shashank@cyware.com>
2019-01-16 08:35:08 +0530

    Added test files

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('test')
+OCconsole.log('test')
+import express from 'express';
+import { blog_data, newsletter_data } from './test_data';
+import { PRODUCTION } from './gulpfile.babel';
+
+require('./gulpfile.babel')
+var bodyParser = require('body-parser');
+var nunjucks = require( 'nunjucks' ) ;
+const routes = require('./routes');
+
+const PORT = 5000;
+const EMAIL_TEMPLATE_API = '/template';
+const app = express();
+const PATH_TO_DIST_TEMPLATES = './dist/social' ;
+
+nunjucks.configure( PATH_TO_DIST_TEMPLATES, {
+  autoescape: false,
+  express: app
+}) ;
+
+app.use(bodyParser.json()); // for parsing application/json
+
+/**
+ * For Development
+ */
+// app.get('/', (req, res) => {
+//   var data = blog_data
+//   res.render('newsletter_blog.html', data)
+// })
+/**
+ * END Development
+ */
+
+app.use(EMAIL_TEMPLATE_API, routes);
+
+app.listen(PORT, () => {
+  console.log(`PRODUCTION: ${PRODUCTION}`)
+  console.log(`server running on port ${PORT}`)
+});
---
+++ route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+import { social_templates } from "./config-template";
+
+/**
+ * Social
+ */
+router.post('/social', (req, res) => {
+  const template = social_templates.find(template => template.template_id === req.body.data.template)
+  res.render(template.template_file, req.body.data)
+})
+
+module.exports = router;
